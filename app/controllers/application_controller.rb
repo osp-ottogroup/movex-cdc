c05d8b494d724aa9abfe87c36e53d30265b06cdb
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2019-12-19 13:50:47 +0100

    Added controller User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+  #protect_from_forgery with: :exception
+  #respond_to :json
+
+
+  protected
+  # switch empty param string to nil
+  def prepare_param(param_sym)
+    retval = params[param_sym]
+    retval = nil if retval == ''
+    retval.strip! unless retval.nil?                                            # Remove leading and trailing blanks
+    retval
+  end
+
 end
