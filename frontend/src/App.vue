1011cfa14c249c7c3fea49dd144f8b8bbfd48e20
Sandro Preuss <Sandro.Preuss@ottogroup.com>
2020-01-15 10:36:40 +0100

    frontend: login form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,46 @@
 <template>
   <div id="app">
-    <router-view/>
+    <template v-if="loggedIn">
+      <router-view/>
+    </template>
+    <template v-else>
+      <b-modal
+        :can-cancel="false"
+        :active="true"
+        :trap-focus="true"
+        :width="400"
+      >
+        <login-form @login="onLogin" />
+      </b-modal>
+    </template>
   </div>
 </template>
+
+<script>
+import LoginForm from './components/LoginForm.vue';
+// import LoginService from './services/LoginService';
+
+export default {
+  name: 'App',
+  components: {
+    LoginForm,
+  },
+  data() {
+    return {
+      loggedIn: false,
+    };
+  },
+  // async created() {
+  //   this.loggedIn = await LoginService.checkLogin();
+  // },
+  methods: {
+    onLogin() {
+      this.loggedIn = true;
+      console.log('login');
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 @import './app.scss';
---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="modal-card" style="width: auto">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Login</p>
+      </header>
+      <section class="modal-card-body">
+        <b-field label="Username">
+          <b-input
+            type="text"
+            :value="userName"
+            placeholder="Your username"
+            required
+          >
+          </b-input>
+        </b-field>
+
+        <b-field label="Password">
+            <b-input
+                type="password"
+                :value="password"
+                password-reveal
+                placeholder="Your password"
+                required>
+            </b-input>
+        </b-field>
+      </section>
+      <footer class="modal-card-foot">
+          <b-button class="button is-primary is-fullwidth" @click="submit">Login</b-button>
+      </footer>
+  </div>
+</template>
+
+<script>
+// import LoginService from '../services/LoginService';
+
+export default {
+  data() {
+    return {
+      userName: null,
+      password: null,
+    };
+  },
+  computed: {
+    credentials() {
+      return {
+        userName: this.userName,
+        password: this.password,
+      };
+    },
+  },
+  methods: {
+    async submit() {
+      try {
+        // await LoginService.login(this.credentials);
+        console.log(this.credentials);
+        this.$emit('login');
+      } catch (e) {
+        this.message = 'Anmeldung nicht erfolgreich!';
+      }
+    },
+  },
+};
+</script>
