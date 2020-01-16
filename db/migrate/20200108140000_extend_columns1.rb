1262badf5dfe3121cada7bed375b18188a30a082
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-01-16 08:57:10 +0100

    Use own foreign key constraint names

--- db/migrate/20200108140000_extend_columns1.rb
+++ db/migrate/20200108140000_extend_columns1.rb
@@ -1,6 +1,6 @@
 class ExtendColumns1 < ActiveRecord::Migration[6.0]
   def change
-    add_foreign_key :columns, :tables
+    add_foreign_key :columns, :tables, name: 'FK_Columns_Tables'
   end
 end
 
