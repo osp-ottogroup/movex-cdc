3eee965408d5c60a196a47916a5236a60276e52d
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-01-23 15:34:14 +0100

    First step trigger generation

--- app/models/trigger_oracle.rb
+++ app/models/trigger_oracle.rb
@@ -36,7 +36,7 @@ class TriggerOracle < TableLess
   def self.generate_triggers(schema_id)
     # TODO: Implement
     target_trigger_data = {}                                                    # Hash with target trigger states for schema
-    Tables.where(schema_id: schema_id).each do |table|
+    Table.where(schema_id: schema_id).each do |table|
       ['I', 'U', 'D'].each do |operation|
 
       end
