3670c3b83849223967863296d36d1bd657e411fb
Sandro Preuss <Sandro.Preuss@ottogroup.com>
2020-01-29 14:44:55 +0100

    backend: enabled cors

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,16 @@ module Trixx
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+            '*',
+            headers: :any,
+            methods: [:get, :patch, :put, :delete, :post, :options]
+        )
+      end
+    end
 
     config.trixx_db_type = ENV['TRIXX_DB_TYPE'] || 'SQLITE'
 
