1cad92ac10297545d839def106a843eb87fef404
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-02-06 21:13:31 +0100

    yml with colon in string

--- app/models/event_log.rb
+++ app/models/event_log.rb
@@ -1,3 +1,4 @@
 class EventLog < ApplicationRecord
-  self.primary_key = "id"                                                       # table does not have real PK constraint
+  self.primary_key    = :id                                                     # table does not have real PK constraint
+  self.sequence_name  = :event_logs_seq
 end
