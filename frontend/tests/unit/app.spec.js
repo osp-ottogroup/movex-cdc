6233e9eb4beda0ac782121998cb2fb84dd969b41
Sandro Preuss <Sandro.Preuss@ottogroup.com>
2020-03-12 14:41:41 +0100

    frontend: first simple component test

---
+++ frontend/tests/unit/app.spec.js
@@ -0,0 +1,21 @@
+import { shallowMount } from '@vue/test-utils';
+import App from '@/App.vue';
+
+describe('App.vue', () => {
+  it('renders login modal if no user is logged in', () => {
+    const wrapper = shallowMount(App, {});
+    console.log('bla', wrapper.html());
+    expect(wrapper.find('b-modal-stub').exists()).toBeTruthy();
+  });
+
+  it('renders application if user is logged in', () => {
+    const wrapper = shallowMount(App, {
+      data() {
+        return {
+          loggedIn: true,
+        };
+      },
+    });
+    expect(wrapper.find('router-view-stub').exists()).toBeTruthy();
+  });
+});
