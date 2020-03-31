89876d3b541296c658b92b87e4f57008e91bbd3a
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-03-31 16:07:49 +0200

    Prepared for docker image

---
+++ docker/trixx/run-trixx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# start TriXX application inside a docker container
+# Peter Ramm, 2020-03-31
+
+# set timezone if requested like TIMEZONE="Europe/Berlin"
+if [ -n "$TIMEZONE" ]; then
+  echo "Setting timezone to $TIMEZONE"
+  echo $TIMEZONE > /etc/timezone
+  rm /etc/localtime && ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+  dpkg-reconfigure -f noninteractive tzdata
+fi
+
+cd /opt/trixx
+export RAILS_LOG_TO_STDOUT=true
+rails server --port 8080 --environment production
+
+
+
+
+
