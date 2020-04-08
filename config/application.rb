0620b01df5f5b02f84a938dae3092e2f3e7670c3
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-04-08 13:58:00 +0200

    fix test db user

--- config/application.rb
+++ config/application.rb
@@ -92,9 +92,9 @@ module Trixx
     case config.trixx_db_type
     when 'ORACLE' then
       if Rails.env.test?                                                        # prevent test-user from overwriting development or production structures in DB
-        config.trixx_db_user            = "test_#{config.trixx_db_user || 'trixx'}"
+        config.trixx_db_user            = "test_#{config.respond_to?(:trixx_db_user) ? config.trixx_db_user : 'trixx'}"
         Trixx::Application.set_and_log_attrib_from_env(:trixx_db_victim_user, 'trixx_victim') if Rails.env.test? # Schema for tables observed by trixx
-        config.trixx_db_system_password = ENV['TRIXX_DB_SYSTEM_PASSWORD'] || 'oracle'
+        Trixx::Application.set_attrib_from_env(:trixx_db_system_password, 'oracle')
       end
       raise "Missing configuration value for 'TRIXX_DB_URL'! Aborting..."       unless config.trixx_db_url
     when 'SQLITE' then
