a4388e83e0de0baf405ac99129e947920d8f0a27
Sandro Preuss <Sandro.Preuss@ottogroup.com>
2020-04-30 16:18:36 +0200

    frontend: use switches instead of checkboxes in column configuration

--- frontend/src/views/configuration/ColumnTable.vue
+++ frontend/src/views/configuration/ColumnTable.vue
@@ -1,44 +1,36 @@
 <template>
   <div>
-    <table class="table is-striped is-hoverable">
-      <thead>
-      <tr>
-        <th>
-          Columns
-        </th>
-        <th>
-          Insert-Trigger
-        </th>
-        <th>
-          Update-Trigger
-        </th>
-        <th>
-          Delete-Trigger
-        </th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="column in columns" :key="column.id">
-        <td>{{ column.name }}</td>
-        <td>
-          <input type="checkbox"
-                 :checked="column.yn_log_insert === 'Y'"
-                 @change="onColumnChanged('insert', column)"
-          >
-        </td>
-        <td>
-          <input type="checkbox"
-                 :checked="column.yn_log_update === 'Y'"
-                 @change="onColumnChanged('update', column)">
-        </td>
-        <td>
-          <input type="checkbox"
-                 :checked="column.yn_log_delete === 'Y'"
-                 @change="onColumnChanged('delete', column)">
-        </td>
-      </tr>
-      </tbody>
-    </table>
+    <b-table ref="table"
+             :data="columns"
+             striped
+             hoverable>
+      <template slot-scope="props">
+        <b-table-column field="name" label="Columns">
+          {{ props.row.name }}
+        </b-table-column>
+        <b-table-column centered field="name" label="Insert-Trigger">
+          <b-switch size="is-small"
+                    true-value="Y"
+                    false-value="N"
+                    v-model="props.row.yn_log_insert"
+                    @input="onColumnChanged(props.row)"/>
+        </b-table-column>
+        <b-table-column centered field="name" label="Update-Trigger">
+          <b-switch size="is-small"
+                    true-value="Y"
+                    false-value="N"
+                    v-model="props.row.yn_log_update"
+                    @input="onColumnChanged(props.row)"/>
+        </b-table-column>
+        <b-table-column centered field="name" label="Delete-Trigger">
+          <b-switch size="is-small"
+                    true-value="Y"
+                    false-value="N"
+                    v-model="props.row.yn_log_delete"
+                    @input="onColumnChanged(props.row)"/>
+        </b-table-column>
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -49,23 +41,8 @@ export default {
     columns: { type: Array, default: () => [] },
   },
   methods: {
-    onColumnChanged(type, column) {
-      switch (type) {
-        case 'insert': column.yn_log_insert = this.yesNoToggle(column.yn_log_insert); break; // eslint-disable-line no-param-reassign
-        case 'update': column.yn_log_update = this.yesNoToggle(column.yn_log_update); break; // eslint-disable-line no-param-reassign
-        case 'delete': column.yn_log_delete = this.yesNoToggle(column.yn_log_delete); break; // eslint-disable-line no-param-reassign
-        default: throw new Error(`Type '${type}' is not supported`);
-      }
+    onColumnChanged(column) {
       this.$emit('column-changed', column);
-    },
-    yesNoToggle(value) {
-      let toggle;
-      if (value === 'Y') {
-        toggle = 'N';
-      } else {
-        toggle = 'Y';
-      }
-      return toggle;
     },
   },
 };
