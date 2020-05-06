2ccefbaa28c02d99e92f226e2e2f75327fce160c
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-05-06 18:45:21 +0200

    Test for configured user only

--- test/models/db_schema_test.rb
+++ test/models/db_schema_test.rb
@@ -9,11 +9,10 @@ class DbSchemaTest < ActiveSupport::TestCase
 
   test "get remaining_schemas" do
     db_schemas = DbSchema.remaining_schemas('quark')                            # non existing user name
-    assert(db_schemas.to_a.include?('name' =>'TRIXX'), 'Should be included in list')
-    assert(db_schemas.to_a.include?('name' =>'TRIXX_VICTIM'), 'Should be included in list')
+    assert(db_schemas.to_a.include?('name' =>Trixx::Application.config.trixx_db_user), 'DB_USER should be included in list')
+    assert(db_schemas.to_a.include?('name' =>Trixx::Application.config.trixx_db_victim_user), 'DB_VICTIM_USER Should be included in list')
 
     db_schemas = DbSchema.remaining_schemas('Peter.Ramm@ottogroup.com')         # existing user name
-    assert(db_schemas.to_a.include?('name' =>'TRIXX'), 'Should be included in list')
     assert(!(db_schemas.to_a.include? 'name' => Trixx::Application.config.trixx_db_user), 'Corresponding schema_right from user should not be in list')
 
   end
