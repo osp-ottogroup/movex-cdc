828f3faa20f64e79e486225b3a80452b63955630
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-05-06 16:25:34 +0200

    Filtered list of schemas

--- app/controllers/db_schemas_controller.rb
+++ app/controllers/db_schemas_controller.rb
@@ -1,7 +1,10 @@
 class DbSchemasController < ApplicationController
+
   # GET /db_schemas
+  # delivers filtered list of schemas really owning tables
+  # schemas already attached to the user are not listed again
   def index
-    @db_schemas = DbSchema.all
+    @db_schemas = DbSchema.remaining_schemas(@current_user)
 
     render json: @db_schemas
   end
--- app/models/db_schema.rb
+++ app/models/db_schema.rb
@@ -1,10 +1,19 @@
 # get available schemas of database
 class DbSchema
 
-  def self.all
+  # delivers filtered list of schemas really owning tables
+  # schemas already attached to the user are not listed again
+  def self.remaining_schemas(current_user)
     case Trixx::Application.config.trixx_db_type
     when 'ORACLE' then
-      ActiveRecord::Base.connection.exec_query("SELECT UserName name FROM All_Users")
+      TableLess.select_all("SELECT DISTINCT Owner Name FROM DBA_Tables
+                            MINUS
+                            SELECT s.Name
+                            FROM   Schemas s
+                            JOIN   Schema_Rights sr ON sr.Schema_ID = s.ID
+                            WHERE  sr.User_ID = :user_id
+                           ", { user_id: current_user.id}
+      )
     when 'SQLITE' then
       [{ 'name' => 'main'}]
     end
