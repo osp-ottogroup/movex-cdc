e4174ed8aac39030c37fcceb8879cce2bb81aac9
Sandro Preuss <Sandro.Preuss@ottogroup.com>
2020-05-19 16:16:09 +0200

    TRIXX-29 frontend: show release info in footer

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -1,17 +1,42 @@
 <template>
   <footer id="app-footer" class="footer">
-    <div class="content has-text-centered">
-      <p>
-        <span>OSP|</span>
-        <strong>Trixx</strong>
-      </p>
+    <div class="columns is-mobile">
+      <div class="column"></div>
+      <div class="column has-text-centered">
+        <p>
+          <span>OSP|</span>
+          <strong>Trixx</strong>
+        </p>
+      </div>
+      <div class="column is-size-7 has-text-right">
+        <span>
+          Release:
+          <i class="release-info">{{releaseInfo}}</i>
+        </span>
+      </div>
     </div>
   </footer>
 </template>
 
 <script>
+import HttpService from '@/services/HttpService';
+import Config from '@/config/config';
+
 export default {
   name: 'AppFooter',
+  data() {
+    return {
+      releaseInfo: null,
+    };
+  },
+  async mounted() {
+    try {
+      const response = await HttpService.get(`${Config.backendUrl}/login/release_info`);
+      this.releaseInfo = response.data.release_info;
+    } catch (e) {
+      // TODO send error to backend
+    }
+  },
 };
 </script>
 
@@ -22,5 +47,8 @@ export default {
    bottom:0px;
    height:30px;
    width:100%;
+}
+.release-info {
+  padding-right: 1rem;
 }
 </style>
