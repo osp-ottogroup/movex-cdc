cf51c78a0de931bdef09c4330cd7d47a813f25ae
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-05-25 10:28:57 +0200

    Move to jruby 9.2.11.1

--- test/models/statistic_counter_test.rb
+++ test/models/statistic_counter_test.rb
@@ -17,7 +17,7 @@ class StatisticCounterTest < ActiveSupport::TestCase
     assert_equal 2,   TableLess.select_one("SELECT SUM(Events_Success) FROM Statistics WHERE Table_ID = 1 AND Operation = 'I'"), 'Table 1 inserts success'
     assert_equal 1,   TableLess.select_one("SELECT SUM(Events_Success) FROM Statistics WHERE Table_ID = 2 AND Operation = 'I'"), 'Table 2 inserts success'
 
-    assert_equal nil, TableLess.select_one("SELECT SUM(Events_Success) FROM Statistics WHERE Table_ID = 1 AND Operation = 'U'"), 'Table 1 updates success'
+    assert_nil TableLess.select_one("SELECT SUM(Events_Success) FROM Statistics WHERE Table_ID = 1 AND Operation = 'U'"), 'Table 1 updates success'
     assert_equal 2,   TableLess.select_one("SELECT SUM(Events_Success) FROM Statistics WHERE Table_ID = 2 AND Operation = 'U'"), 'Table 2 updates success'
 
     assert_equal 1,   TableLess.select_one("SELECT SUM(Events_Success) FROM Statistics WHERE Table_ID = 1 AND Operation = 'D'"), 'Table 1 updates deletes'
@@ -36,7 +36,7 @@ class StatisticCounterTest < ActiveSupport::TestCase
     assert_equal 2,   TableLess.select_one("SELECT SUM(Events_Failure) FROM Statistics WHERE Table_ID = 1 AND Operation = 'I'"), 'Table 1 inserts failure'
     assert_equal 1,   TableLess.select_one("SELECT SUM(Events_Failure) FROM Statistics WHERE Table_ID = 2 AND Operation = 'I'"), 'Table 2 inserts failure'
 
-    assert_equal nil, TableLess.select_one("SELECT SUM(Events_Failure) FROM Statistics WHERE Table_ID = 1 AND Operation = 'U'"), 'Table 1 updates failure'
+    assert_nil TableLess.select_one("SELECT SUM(Events_Failure) FROM Statistics WHERE Table_ID = 1 AND Operation = 'U'"), 'Table 1 updates failure'
     assert_equal 2,   TableLess.select_one("SELECT SUM(Events_Failure) FROM Statistics WHERE Table_ID = 2 AND Operation = 'U'"), 'Table 2 updates failure'
 
     assert_equal 1,   TableLess.select_one("SELECT SUM(Events_Failure) FROM Statistics WHERE Table_ID = 1 AND Operation = 'D'"), 'Table 1 updates failure'
