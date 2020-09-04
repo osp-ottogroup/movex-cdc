feb9f36b29db0228e2cbfe4e3923a470638ab36d
Sandro Preuss <Sandro.Preuss@ottogroup.com>
2020-09-04 11:12:31 +0200

    TRIX-71 frontend: css improved; use search-event of input instead of explicit buttons

--- frontend/src/views/administration/ServerLogViewer.vue
+++ frontend/src/views/administration/ServerLogViewer.vue
@@ -1,26 +1,19 @@
 <template>
-  <div class="mx-6">
+  <div class="mx-6 server-logs">
     <p class="title is-5">Server Log</p>
 
     <b-field grouped>
       <b-input placeholder="Filter..."
                type="search"
                icon="magnify"
-               v-model="filter">
+               v-model="filter"
+               @search.native="search">
       </b-input>
-      <p class="control">
-        <button class="button is-primary" @click="filterData">Filter</button>
-      </p>
-      <p class="control">
-        <button class="button is-primary" @click="clearFilter">Clear</button>
-      </p>
     </b-field>
 
-    <div class="logs-wrapper">
-      <div class="logs">
-        {{data}}
-      </div>
+    <div class="logs">
       <b-loading :active="isLoading" :is-full-page="false"></b-loading>
+      {{data}}
     </div>
   </div>
 </template>
@@ -45,15 +38,24 @@ export default {
     this.isLoading = false;
   },
   methods: {
+    search() {
+      if (this.filter === '') {
+        this.clearFilter();
+      } else {
+        this.filterData();
+      }
+    },
     filterData() {
       this.isLoading = true;
       if (this.filter === '') {
-        return;
+        this.data = this.completeData;
+        this.isLoading = false;
+      } else {
+        const regex = new RegExp(`^.*${this.filter}.*$`, 'gmi');
+        const result = this.completeData.match(regex);
+        this.data = result ? result.join('\n') : '';
+        this.isLoading = false;
       }
-      const regex = new RegExp(`^.*${this.filter}.*$`, 'gmi');
-      const result = this.completeData.match(regex);
-      this.data = result ? result.join('\n') : '';
-      this.isLoading = false;
     },
     clearFilter() {
       // TODO copy of huge strings seems to be slow, using loading indicator, maybe using a reference is possible
@@ -67,12 +69,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../../app.scss";
+
+.server-logs {
+  height: calc(100vh - #{$navbar-height} - 30px - 2rem);
+  display: flex;
+  flex-direction: column;
+}
 .logs {
   white-space: pre;
-  overflow: auto;
-  height: 80vh;
-}
-.logs-wrapper {
   position: relative;
+  overflow: auto;
+  flex-grow: 1;
 }
 </style>
