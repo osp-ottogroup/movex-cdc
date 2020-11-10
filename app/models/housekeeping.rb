9a061c79e3233c7b2b55b807d8a0ec23dbcd9cbd
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-11-10 12:19:50 +0100

    Wrong time comparison removed (UTC vs. local time as result from Time.parse)

--- app/models/housekeeping.rb
+++ app/models/housekeeping.rb
@@ -10,10 +10,10 @@ class Housekeeping
     if @last_housekeeping_started.nil?
       Rails.logger.debug "Housekeeping.do_housekeeping: Start housekeeping"
       do_housekeeping_internal
-      true
+      true                                                                      # signal state for test run only
     else
       Rails.logger.error "Housekeeping.do_housekeeping: Last run started at #{@last_housekeeping_started} not yet finished!"
-      false
+      false                                                                     # signal state for test run only
     end
   end
 
@@ -37,32 +37,28 @@ class Housekeeping
                            )
         SELECT Partition_Name, High_Value
         FROM   Partitions
-        WHERE  Partition_Position != (SELECT MAX(Partition_Position) FROM Partitions)
+        WHERE  Partition_Position != (SELECT MAX(Partition_Position) FROM Partitions) /* do not check the youngest partition for deletion */
         "
       ).each do |part|
-        part_high_value_ts = Time.parse(part['high_value'])
-        oldest_high_value = Database.select_one "SELECT SYSDATE - 1/24 FROM DUAL" # Use time from DB because of possibe different time zone compared to client
-        if part_high_value_ts < oldest_high_value                               # Check partitions only with high_value older than x hours
-          Rails.logger.info "Housekeeping: Check partition #{part['partition_name']} with high value #{part['high_value']} for deletion"
-          pending_transactions = Database.select_one("\
-            SELECT COUNT(*)
-            FROM   gv$Lock l
-            JOIN   All_Objects o ON o.Object_ID = l.ID1
-            WHERE  o.Object_Name    = 'EVENT_LOGS'
-            AND    o.SubObject_Name = :partition_name
-            ", partition_name: part['partition_name']
-          )
-          if pending_transactions > 0
-            Rails.logger.info "Housekeeping: Drop partition #{part['partition_name']} with high value #{part['high_value']} not possible because there are #{pending_transactions} pending transactions"
+        Rails.logger.info "Housekeeping: Check partition #{part['partition_name']} with high value #{part['high_value']} for deletion"
+        pending_transactions = Database.select_one("\
+          SELECT COUNT(*)
+          FROM   gv$Lock l
+          JOIN   All_Objects o ON o.Object_ID = l.ID1
+          WHERE  o.Object_Name    = 'EVENT_LOGS'
+          AND    o.SubObject_Name = :partition_name
+          ", partition_name: part['partition_name']
+        )
+        if pending_transactions > 0
+          Rails.logger.info "Housekeeping: Drop partition #{part['partition_name']} with high value #{part['high_value']} not possible because there are #{pending_transactions} pending transactions"
+        else
+          existing_records = Database.select_one "SELECT COUNT(*) FROM Event_Logs PARTITION (#{part['partition_name']})"
+          if existing_records > 0
+            Rails.logger.info "Housekeeping: Drop partition #{part['partition_name']} with high value #{part['high_value']} not possible because there are #{existing_records} records remaining"
           else
-            existing_records = Database.select_one "SELECT COUNT(*) FROM Event_Logs PARTITION (#{part['partition_name']})"
-            if existing_records > 0
-              Rails.logger.info "Housekeeping: Drop partition #{part['partition_name']} with high value #{part['high_value']} not possible because there are #{existing_records} records remaining"
-            else
-              Rails.logger.info "Housekeeping: Execute drop partition #{part['partition_name']} with high value #{part['high_value']}"
-              Database.execute "ALTER TABLE Event_Logs DROP PARTITION #{part['partition_name']}"
-              Rails.logger.info "Housekeeping: Successful dropped partition #{part['partition_name']} with high value #{part['high_value']}"
-            end
+            Rails.logger.info "Housekeeping: Execute drop partition #{part['partition_name']} with high value #{part['high_value']}"
+            Database.execute "ALTER TABLE Event_Logs DROP PARTITION #{part['partition_name']}"
+            Rails.logger.info "Housekeeping: Successful dropped partition #{part['partition_name']} with high value #{part['high_value']}"
           end
         end
       end
