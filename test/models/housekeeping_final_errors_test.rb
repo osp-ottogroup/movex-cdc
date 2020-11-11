4b3e2bac705d27c28eb69dfd8b52b43c1f9c9e46
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-11-11 14:58:34 +0100

    Really commit test data insert for event_log_final_errors: Limitation for SQLIte

--- test/models/housekeeping_final_errors_test.rb
+++ test/models/housekeeping_final_errors_test.rb
@@ -10,7 +10,9 @@ class HousekeepingFinalErrorsTest < ActiveSupport::TestCase
     Database.execute "INSERT INTO Event_Log_Final_Errors (ID, Table_ID, Operation, DBUser, Payload, Created_At, Error_Time, Error_Msg)
                     VALUES (-2, 1, 'I', 'HUGO', '{}', :created_at, :error_time, 'Test-Error to keep')
                    ", {created_at: 100.day.ago, error_time: 2.day.ago}
-    Database.execute "COMMIT"                                                   # Ensure the previous Inserts are really commited in test environment
+
+    # Ensure the previous Inserts are really commited in test environment! ActiveRecord::Base.transaction does not do this in test environment.
+    Database.execute "COMMIT" if Trixx::Application.config.trixx_db_type == 'ORACLE'
 
     retval = HousekeepingFinalErrors.get_instance.do_housekeeping
     assert(retval, 'Should not return false')
