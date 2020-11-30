b3374994b0bf86d1849a1d275a7b69ec3f9520cb
Sandro Preuss <Sandro.Preuss@ottogroup.com>
2020-11-30 14:24:32 +0100

    TRIX-18 frontend: loading indicator for home screen; explicit contact-person component

---
+++ frontend/src/components/ContactPerson.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-if="contactPerson !== ''" class="has-text-grey-light mb-1">
+      If you have questions regarding this Instance of TriXX, so please contact <span class="has-text-grey">{{ contactPerson }}</span>.
+    </div>
+    <div class="has-text-grey-light">
+      Follow <router-link to="information">this link</router-link> to get more information about this TriXX instance.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactPerson',
+  props: {
+    contactPerson: { type: String, default: '' },
+  },
+};
+</script>
