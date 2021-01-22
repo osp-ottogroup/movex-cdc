cbc625928dea8dbb10476d0884bae21e4eb8cc07
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2021-01-22 14:32:03 +0100

    Provide trixx.html and trixx.pdf as public files

--- test/controllers/help_controller_test.rb
+++ test/controllers/help_controller_test.rb
@@ -1,15 +1,13 @@
 require 'test_helper'
 
 class HelpControllerTest < ActionDispatch::IntegrationTest
-  test "should get doc_html" do
-    FileUtils.cp Rails.root.join('test/fixtures/files/trixx.html.testfile'), Rails.root.join('doc/trixx.html')
-    get "/help/doc_html", as: :html
+  test "should get html doc" do
+    get "/trixx.html", as: :html
     assert_response :success
   end
 
-  test "should get doc_pdf" do
-    FileUtils.cp Rails.root.join('test/fixtures/files/trixx.pdf.testfile'), Rails.root.join('doc/trixx.pdf')
-    get "/help/doc_pdf", as: :html
+  test "should get pdf doc" do
+    get "/trixx.pdf", as: :html
     assert_response :success
   end
 
