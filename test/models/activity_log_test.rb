d222e9b7a7418c43bac55339174b4f5ceb57c90f
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2021-05-12 13:22:30 +0200

    Without fixture activity_logs.yml reduces test time

--- test/models/activity_log_test.rb
+++ test/models/activity_log_test.rb
@@ -4,6 +4,8 @@ class ActivityLogTest < ActiveSupport::TestCase
 
   test "create activity_log" do
     ActivityLog.new(user_id: 1, schema_name: 'Schema1', table_name: 'Table1', column_name: 'Column1', action: 'info').save!
+    ActivityLog.new(user_id: 1, schema_name: 'Schema1', table_name: 'Table1', action: 'Without column').save!
+    ActivityLog.new(user_id: 1, schema_name: 'Schema1', action: 'Without table').save!
   end
 
 end
