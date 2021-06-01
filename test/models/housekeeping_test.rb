217c537e76b6158c592eb8728d6cf8a535d13d38
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2021-06-01 17:44:23 +0200

    Reduced logging

--- test/models/housekeeping_test.rb
+++ test/models/housekeeping_test.rb
@@ -11,11 +11,11 @@ class HousekeepingTest < ActiveSupport::TestCase
     when 'ORACLE' then
       if Trixx::Application.partitioning?
 
-        log_state = proc do
+        log_state = proc do |console|
           Database.select_all("SELECT * FROM User_Tab_Partitions WHERE Table_Name = 'EVENT_LOGS' ORDER BY Partition_Position").each do |p|
             msg = "Partition #{p.partition_name} Pos=#{p.partition_position} High_Value=#{p.high_value} Interval=#{p.interval} Position=#{p.partition_position}"
             Rails.logger.debug msg
-            puts msg
+            puts msg if console
           end
         end
 
@@ -33,7 +33,7 @@ class HousekeepingTest < ActiveSupport::TestCase
             current_high_value_time = get_time_from_high_value.call
             if current_high_value_time >= high_value_time                 # high value should by adjusted to an older Time
               Rails.logger.debug "high value should by adjusted to an older Time: current=#{current_high_value_time}, expected=#{high_value_time}"
-              log_state.call                                                    # log partitions
+              log_state.call(false)                                             # log partitions
               Database.execute "ALTER TABLE Event_Logs SET INTERVAL ()"         # Workaround bug in 12.1.0.2 where oldest range partition cannot be dropped if split is done with older high_value (younger partition can be dropped instead)
               partition_name = Database.select_one "SELECT Partition_Name FROM User_Tab_Partitions WHERE Table_Name = 'EVENT_LOGS' AND Partition_Position = 1"
               # Remove all range partitions except the MIN partition
@@ -46,7 +46,7 @@ class HousekeepingTest < ActiveSupport::TestCase
                               PARTITION TestSplit2)"
               Database.execute "ALTER TABLE Event_Logs RENAME PARTITION TestSplit1 TO MIN"
               Database.execute "ALTER TABLE Event_Logs DROP PARTITION TestSplit2"
-              log_state.call                                                    # log partitions
+              log_state.call(false)                                             # log partitions
             end
             EventLog.adjust_interval                                            # adjust in DB according to Trixx::Application.config.trixx_partition_interval
 
@@ -63,7 +63,7 @@ class HousekeepingTest < ActiveSupport::TestCase
             max_minutes_for_interval_prev= 700000*prev_interval                 # > 1/2 of max. partition count (1024*1024-1) for default interval
             set_high_value.call(Time.now-max_minutes_for_interval_prev*60, prev_interval) # set old high_value to 1/2 of possible partition count and default interval
             Housekeeping.get_instance.check_partition_interval
-            log_state.call                                                        # log partitions
+            log_state.call(false)                                               # log partitions
 
             current_hv = get_time_from_high_value.call
             max_expected_minutes_for_interval = (1024*1024)/6*interval          # < 1/4 of max. partition count (1024*1024-1) for interval
@@ -79,7 +79,7 @@ class HousekeepingTest < ActiveSupport::TestCase
           do_check.call(2000, 200)                                              # should change only high_value
 
         rescue
-          log_state.call                                                        # log partitions
+          log_state.call(true)                                                  # log partitions
           raise
         end
       end
