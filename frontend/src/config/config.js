a50cf4d19f68ba9d7e9dc57ca401c9fddd74b131
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-04-25 16:19:21 +0200

    Overwrite backend URL by <base> tag for nginx with sub locations

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -2,8 +2,8 @@ const { protocol, host } = window.location;
 
 // if <base href="http://host:port/path" is set in html head section then
 // - the href from <base> is used as backend URL
-// - all relative links are suffixed with host:port of the baseHref if the relative link starts with /
-// - all relative links are suffixed with the whole baseHref if relate link doe not start with /
+// - all relative links are preixed with host:port of the baseHref if the relative link starts with /
+// - all relative links are prefixed with the whole baseHref if relate link doe not start with /
 // used to fix location problems in nginx or apache by injecting a <base> tag in head section
 const baseHref = (document.getElementsByTagName('base')[0] || {}).href;
 
