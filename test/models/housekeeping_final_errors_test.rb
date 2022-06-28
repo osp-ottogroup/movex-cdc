b7e4ed5b2391979920a72d698c568bca2d721e87
Peter Ramm <peter.ramm@ottogroup.com>
2022-06-28 11:01:39 +0000

    Harden tests for partition housekeeping

--- test/models/housekeeping_final_errors_test.rb
+++ test/models/housekeeping_final_errors_test.rb
@@ -5,10 +5,10 @@ class HousekeepingFinalErrorsTest < ActiveSupport::TestCase
   test "do_housekeeping" do
     Database.execute "DELETE FROM Event_Log_Final_Errors"
     Database.execute "INSERT INTO Event_Log_Final_Errors (ID, Table_ID, Operation, DBUser, Payload, Created_At, Error_Time, Error_Msg)
-                    VALUES (-1, 1, 'I', 'HUGO', '{}', :created_at, :error_time, 'Test-Error to delete')
+                    VALUES (-1, 1, 'I', 'HUGO', '\"new\": { \"ID\": 1}', :created_at, :error_time, 'Test-Error to delete')
                    ", binds: {created_at: 100.day.ago, error_time: 100.day.ago}
     Database.execute "INSERT INTO Event_Log_Final_Errors (ID, Table_ID, Operation, DBUser, Payload, Created_At, Error_Time, Error_Msg)
-                    VALUES (-2, 1, 'I', 'HUGO', '{}', :created_at, :error_time, 'Test-Error to keep')
+                    VALUES (-2, 1, 'I', 'HUGO', '\"new\": { \"ID\": 1}', :created_at, :error_time, 'Test-Error to keep')
                    ", binds: {created_at: 100.day.ago, error_time: 2.day.ago}
 
     # Ensure the previous Inserts are really commited in test environment! ActiveRecord::Base.transaction does not do this in test environment.
