a4dbf39325a25621beebd016468eb2a6e8fd2558
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-06-29 09:46:45 +0200

    Store material design files in md_css to not cover the needed path css

--- docker/movex-cdc/run-movex-cdc.sh
+++ docker/movex-cdc/run-movex-cdc.sh
@@ -87,12 +87,12 @@ export PUBLIC_PATH
 (
   cd public
   # TODO: move material design icons to npm, up to then adjust the path for link and font files
-  # Replace the public path in generated css and js links of index.html and in js files
+  # Replace the public path in generated md_css and js links of index.html and in js files
   sed -i "
     s/\/REPLACE_PUBLIC_PATH_BEFORE/$PUBLIC_PATH/g
-    s/css\//$PUBLIC_PATH\/css\//g
+    s/md_css\//$PUBLIC_PATH\/md_css\//g
     " index.html
-  sed -i "s/\.\.\/fonts/$PUBLIC_PATH\/fonts/g" css/materialdesignicons_5.4.55.min.css
+  sed -i "s/\.\.\/fonts/$PUBLIC_PATH\/fonts/g" md_css/materialdesignicons_5.4.55.min.css
   cd js
   sed -i "s/\/REPLACE_PUBLIC_PATH_BEFORE/$PUBLIC_PATH/g" *
 )
