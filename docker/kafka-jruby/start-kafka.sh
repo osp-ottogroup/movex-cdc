bd0f3ddb97a503d725658048610f8dd40ad12104
Peter Ramm <Peter.Ramm@ottogroup.com>
2023-07-10 15:48:14 +0200

    Create Kafka groups in Background

--- docker/kafka-jruby/start-kafka.sh
+++ docker/kafka-jruby/start-kafka.sh
@@ -9,6 +9,7 @@ export KAFKA_LISTENERS=LISTENER_EXT://0.0.0.0:9092,LISTENER_INT://0.0.0.0:9093
 export KAFKA_ADVERTISED_LISTENERS=LISTENER_EXT://localhost:9092,LISTENER_INT://localhost:9093
 export KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=LISTENER_EXT:PLAINTEXT,LISTENER_INT:PLAINTEXT
 export KAFKA_INTER_BROKER_LISTENER_NAME=LISTENER_INT
+export WAIT_FOR_KAFKA_SECS=60
 
 echo "Prepare configuration"
 sed -i "s|^broker.id=.*$|broker.id=$BROKER_ID|" $KAFKA_HOME/config/server.properties
@@ -38,9 +39,9 @@ do
     echo "Following double output 'org.apache.kafka.common.errors.TimeoutException' is 'works as designed'"
     $KAFKA_HOME/bin/kafka-topics.sh --create --topic TestTopic1 --partitions 4 --bootstrap-server localhost:9092 --replication-factor 1
     $KAFKA_HOME/bin/kafka-topics.sh --create --topic TestTopic2 --partitions 8 --bootstrap-server localhost:9092 --replication-factor 1
-    $KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic TestTopic1 --group Group1 --timeout-ms 30000 &
-    $KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic TestTopic1 --group Group2 --timeout-ms 30000 &
-    typeset -i GROUP_LOOP_COUNT = 0
+    $KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic TestTopic1 --group Group1 --timeout-ms ${WAIT_FOR_KAFKA_SECS}000 &
+    $KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic TestTopic1 --group Group2 --timeout-ms ${WAIT_FOR_KAFKA_SECS}000 &
+    typeset -i GROUP_LOOP_COUNT=0
     while [ 1 -eq 1 ]
     do
       GROUP_COUNT=`$KAFKA_HOME/bin/kafka-consumer-groups.sh --bootstrap-server=localhost:9092 --list | wc -l`
@@ -50,8 +51,8 @@ do
         exit 0
       fi
       GROUP_LOOP_COUNT=$GROUP_LOOP_COUNT+1
-      if [ $GROUP_LOOP_COUNT -gt 30 ]; then
-        echo "Two Kafka groups missing after 30 seconds, terminating"
+      if [ $GROUP_LOOP_COUNT -gt $WAIT_FOR_KAFKA_SECS ]; then
+        echo "Two Kafka groups missing after $WAIT_FOR_KAFKA_SECS seconds, terminating"
         $KAFKA_HOME/bin/kafka-consumer-groups.sh --bootstrap-server=localhost:9092 --list
         exit 1
       fi
@@ -61,9 +62,9 @@ do
   fi
 
   LOOP_COUNT=$LOOP_COUNT+1
-  if [ $LOOP_COUNT -gt 20 ]; then
+  if [ $LOOP_COUNT -gt $WAIT_FOR_KAFKA_SECS ]; then
     echo ""
-    echo "Kafka not in operation after 20 seconds, terminating"
+    echo "Kafka not in operation after $WAIT_FOR_KAFKA_SECS seconds, terminating"
     echo ""
     echo "############# Zookeeper log ##############"
     cat $KAFKA_HOME/logs/zookeeper.out
