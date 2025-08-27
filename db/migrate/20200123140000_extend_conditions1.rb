e6c1143575e02f2f87cd405916e864dd107ac84e
Peter Ramm <Peter.Ramm@ottogroup.com>
2025-08-27 16:24:07 +0200

    Fix test migration

--- db/migrate/20200123140000_extend_conditions1.rb
+++ db/migrate/20200123140000_extend_conditions1.rb
@@ -1,6 +1,6 @@
 class ExtendConditions1 < ActiveRecord::Migration[6.0]
   def change
-    add_foreign_key :conditions, :tables, name: 'fk_conditions_tables'
+    add_foreign_key :conditions, :tables, name: 'fk_conditions_table', index: false
   end
 end
 
