4a7896c7c839e7129d017db311e42b1674ff625f
Peter Ramm <Peter.Ramm@ottogroup.com>
2025-08-27 16:38:27 +0200

    Fix test too long index name

--- db/migrate/20200114103309_create_schema_rights.rb
+++ db/migrate/20200114103309_create_schema_rights.rb
@@ -2,8 +2,8 @@ class CreateSchemaRights < ActiveRecord::Migration[6.0]
 
   def change
     create_table :schema_rights do |t|
-      t.references  :user,                        null: false,              comment: 'Reference to user'
-      t.references  :schema,                      null: false,              comment: 'Reference to schema'
+      t.references  :user,null: false, index: false, comment: 'Reference to user'
+      t.references  :schema,null: false, index: { name: "FK_SCHEMA_RIGHTS_SCHEMA"}, comment: 'Reference to schema'
       t.string      :info,          limit: 1000,  null: true,               comment: 'Additional info'
       t.integer     :lock_version,                null: false,  default: 0, comment: 'Version for optimistic locking'
       t.timestamps
