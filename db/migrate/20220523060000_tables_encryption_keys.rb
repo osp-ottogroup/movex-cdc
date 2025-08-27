9630446b5effa831ea701d414e143e66f8932551
Peter Ramm <Peter.Ramm@ottogroup.com>
2025-08-27 17:20:09 +0200

    Fix test shorten index name

--- db/migrate/20220523060000_tables_encryption_keys.rb
+++ db/migrate/20220523060000_tables_encryption_keys.rb
@@ -1,5 +1,5 @@
 class TablesEncryptionKeys < ActiveRecord::Migration[6.0]
   def change
-    add_reference :tables, :encryption_key, foreign_key: true, null:true
+    add_reference :tables, :encryption_key, null:true, foreign_key: { name: "fk_tables_encryption_key"}, index: { name: 'IX_TABLES_ENCRYPTION_KEY' }
   end
 end
