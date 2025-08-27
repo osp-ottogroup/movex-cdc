bbf8648541bf3a0a11979503c13ed26952a36b76
Peter Ramm <Peter.Ramm@ottogroup.com>
2025-08-27 13:06:40 +0200

    Fix test

--- test/controllers/conditions_controller_test.rb
+++ test/controllers/conditions_controller_test.rb
@@ -28,22 +28,22 @@ class ConditionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show condition" do
-    get condition_url(@condition), headers: jwt_header, as: :json
+    get conditions_url(@condition), headers: jwt_header, as: :json
     assert_response :success
 
-    get condition_url(@condition), headers: jwt_header(@jwt_no_schema_right_token), as: :json
+    get conditions_url(@condition), headers: jwt_header(@jwt_no_schema_right_token), as: :json
     assert_response :internal_server_error, log_on_failure('Should not get access without schema rights')
   end
 
   test "should update condition" do
     org_filter = @condition.filter
-    patch condition_url(@condition), headers: jwt_header, params: { condition: { filter: 'new filter', lock_version: @condition.lock_version } }, as: :json
+    patch conditions_url(@condition), headers: jwt_header, params: { condition: { filter: 'new filter', lock_version: @condition.lock_version } }, as: :json
     assert_response 200
 
     condition = Condition.find(@condition.id)                                   # load fresh state from DB
     run_with_current_user { condition.update!(filter: org_filter, lock_version: condition.lock_version) } # Restore previous state
 
-    patch condition_url(@condition), headers: jwt_header(@jwt_no_schema_right_token), params: { condition: {  } }, as: :json
+    patch conditions_url(@condition), headers: jwt_header(@jwt_no_schema_right_token), params: { condition: {  } }, as: :json
     assert_response :internal_server_error, log_on_failure('Should not get access without schema rights')
   end
 
@@ -52,18 +52,18 @@ class ConditionsControllerTest < ActionDispatch::IntegrationTest
     run_with_current_user { condition_to_delete.save! }
 
     assert_difference('Condition.count', -1) do
-      delete condition_url(condition_to_delete), headers: jwt_header, params: { condition: condition_to_delete.attributes}, as: :json
+      delete conditions_url(condition_to_delete), headers: jwt_header, params: { condition: condition_to_delete.attributes}, as: :json
     end
     assert_response 204
 
     condition = Condition.where(table_id: victim1_table.id, operation: 'D').first
-    delete condition_url(condition), headers: jwt_header, params: { condition: {lock_version: 42}}, as: :json
+    delete conditions_url(condition), headers: jwt_header, params: { condition: {lock_version: 42}}, as: :json
     assert_response :internal_server_error
     assert response.body['ActiveRecord::StaleObjectError'], log_on_failure('Should raise ActiveRecord::StaleObjectError')
   end
 
   test "should not destroy condition" do
-    delete condition_url(@condition), headers: jwt_header(@jwt_no_schema_right_token), params: { condition: @condition.attributes}, as: :json
+    delete conditions_url(@condition), headers: jwt_header(@jwt_no_schema_right_token), params: { condition: @condition.attributes}, as: :json
     assert_response :internal_server_error, log_on_failure('Should not get access without schema rights')
   end
 
