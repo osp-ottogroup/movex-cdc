afdb2d63a76244017a3e6155c57cd8815e1fdca4
Peter Ramm <Peter.Ramm@ottogroup.com>
2025-09-03 14:47:34 +0200

    Show duplicate instances only as error in log but do not cancel initialization

--- app/models/heartbeat.rb
+++ app/models/heartbeat.rb
@@ -2,6 +2,7 @@ require 'socket'
 
 class Heartbeat < ApplicationRecord
   @@ip_address = nil
+  @@known_concurrent_instances = {}                                             # Check for known concurrent instances to avoid repeated warnings
 
   # Create or update the heartbeat record for the current server instance
   # Uses hostname and IP address to identify the server instance
@@ -16,33 +17,36 @@ class Heartbeat < ApplicationRecord
 
   def self.check_for_concurrent_instance
     # Raise exception if there are other heartbeat records within the last 5 minutes
-    others = Heartbeat.where("(HostName != ? OR IP_Address != ?) AND Heartbeat_TS > ?", Socket.gethostname, current_ip_address, 5.minutes.ago)
+    others = Heartbeat.where("(HostName != ? OR IP_Address != ?) AND Heartbeat_TS > ?", Socket.gethostname, current_ip_address, 2.minutes.ago)
     if !others.empty?
-      msg = "Foreign heartbeat records found for this DB schema other than (#{Socket.gethostname}/#{current_ip_address}): #{others.map { |h| "#{h.hostname}/#{h.ip_address} (last heartbeat at #{h.heartbeat_ts})" }.join(', ')}! Only one server instance is allowed to run with the same DB schema at a time!"
+      msg = "Foreign heartbeat record found younger than 2 minutes for this DB schema other than (#{Socket.gethostname}/#{current_ip_address}): #{others.map { |h| "#{h.hostname}/#{h.ip_address} (last heartbeat at #{h.heartbeat_ts})" }.join(', ')}!\n" +
+            " Only one server instance is allowed to run with the same DB schema at a time!\n"+
+            "This might be a false positive if the instance was recreated in a new Docker container."
       Rails.logger.error("Heartbeat.check_for_concurrent_instance") { msg }
-      raise msg
     end
 
-    # Log error if there are other heartbeat records within the last 24 hours
+    # Log error if there are other heartbeat records within the last 24 hours, log only one time if the heartbeat timestamp does not change
     # This should catch possible timezone issues where the other heartbeat record is older than 5 minutes but still from the same day
     # This is just a warning and does not raise an exception, because it might be a false positive if the instance was moved to another server
     others = Heartbeat.where("(HostName != ? OR IP_Address != ?) AND Heartbeat_TS > ?", Socket.gethostname, current_ip_address, 24.hours.ago)
     if !others.empty?
-      msg = "Foreign heartbeat records of the last 24 hours found for this DB schema other than (#{Socket.gethostname}/#{current_ip_address}): #{others.map { |h| "#{h.hostname}/#{h.ip_address} (last heartbeat at #{h.heartbeat_ts})" }.join(', ')}! " +
-            "Only one server instance is allowed to run with the same DB schema at a time! " +
-            "This might be a false positive if the instance was recreated in a new Docker container."
-      Rails.logger.warn("Heartbeat.check_for_concurrent_instance") { msg }
+      others.each do |o|
+        key = "#{o.hostname}/#{o.ip_address}"
+        if !(@@known_concurrent_instances.key?(key) && @@known_concurrent_instances[key] == o.heartbeat_ts)
+          @@known_concurrent_instances[key] = o.heartbeat_ts                    # Remenber the known concurrent instance to avoid repeated warnings
+          msg = "Foreign heartbeat records of the last 24 hours found for this DB schema other than (#{Socket.gethostname}/#{current_ip_address}): #{o.hostname}/#{o.ip_address} (last heartbeat at #{o.heartbeat_ts})!\n" +
+                "Only one server instance is allowed to run with the same DB schema at a time!\n" +
+                "This might be a false positive if the instance was recreated in a new Docker container."
+          Rails.logger.warn("Heartbeat.check_for_concurrent_instance") { msg }
+        end
+      end
     end
-
   end
 
   # Get the IP address of the current database session
   # Cache the value in a class variable to avoid repeated queries
   # @return [String] IP address
   def self.current_ip_address
-
-
-
     if @@ip_address.nil?
       @@ip_address = case MovexCdc::Application.config.db_type
                      when 'ORACLE' then
