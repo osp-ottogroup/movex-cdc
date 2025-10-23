2249520a5abac5a455b5b85272e29da6c598a186
Peter Ramm <Peter.Ramm@ottogroup.com>
2025-10-23 12:38:13 +0200

    Test fixed

--- test/models/table_test.rb
+++ test/models/table_test.rb
@@ -55,9 +55,6 @@ class TableTest < ActiveSupport::TestCase
 
   test "table validations" do
     run_with_current_user do
-      result = Table.find(tables_table.id).update(kafka_key_handling: 'N', fixed_message_key: 'hugo')
-      assert !result, log_on_failure('Validation should raise error for fixed_message_key if not empty')
-
       result = Table.find(tables_table.id).update(kafka_key_handling: 'F', fixed_message_key: nil)
       assert !result, log_on_failure('Validation should raise error for fixed_message_key if empty')
 
